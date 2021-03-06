9b6906b96bc34ce6d65d3065a832fde07dc86e88
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2021-03-06 18:17:43 +0100

    exercise conjunto char con elementos ordenados crecientemente done

--- conjunto_char_con_elementos_ordenados/main.cpp
+++ conjunto_char_con_elementos_ordenados/main.cpp
@@ -1,23 +1,48 @@
+#include<iostream>
+#include<algorithm>
+#include <cassert>
+#include <iomanip>
+#include <fstream>
 
 
 
+using namespace std;
+
+const int MAX_CHARS = 1000;
 
 class ConjuntoChar {
 public:
 	ConjuntoChar();
-	bool pertenece(char letra) const;
-	void anyadir(char letra);
+	bool pertenece(char letra) const {
+		return binary_search(elems, elems + num_elems, letra);
+	}
+	void anyadir(char letra) { //Anyadimos de forma ordenada creciente
+		int i = 0;
+		while (i < num_elems && elems[i] < letra) { //Para saber donde añadir
+			i++;
+		}
+
+		if (letra != elems[i]) { //No anyadimos letras repetidas
+			for (int j = num_elems; j > i; j--) { //Por si toca insertar en un hueco ocupado
+				elems[j] = elems[j - 1];
+			}
+			//Anyadimos
+			elems[i] = letra;
+			num_elems++;
+		}
+	}
 private:
 	int num_elems;
 	char elems[MAX_CHARS];
 };
 
+//El invariante nos dice que estan ordenados de menor a mayor
+//Ademas no hay elementos repetidos
 
 
 
 
-
-int main{
-
+int main(){
+	//TODO en el futuro, el codigo de arriba es correcto
 
 }
