3400650c2ab9f2e52acf975e68a599926e795884
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2021-06-03 15:55:42 +0200

    ejercicio de juez hecho, invertir una lista enlazada

---
+++ reverse_lista_enlazada/Source.cpp
@@ -0,0 +1,57 @@
+﻿// Nombre del alumno: Pablo Lopez Martin
+// Usuario del Juez: F36
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "ListLinkedSingle.h"
+
+// función que resuelve el problema
+void resolver(ListLinkedSingle<int> &lista) {
+    //Duplicar elementos de la lista 1 a 1
+    lista.reverse();
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    ListLinkedSingle<int> lista;
+    int aux;
+   std:: cin >> aux;
+    while (aux != 0) {
+        lista.push_back(aux);
+       std:: cin >> aux;
+    }
+
+    resolver(lista);
+    // escribir sol
+    lista.display();
+    std::cout << std::endl;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("1.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
