daba9ac47bb794e5737da77ff2499965789740b8
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2021-06-12 12:52:14 +0200

    ejercicio Juez arboles zurdos, hecho y correcto

--- arboles_zurdos/Source.cpp
+++ arboles_zurdos/Source.cpp
@@ -1,41 +1,84 @@
+﻿
+
+// Nombre del alumno: Pablo Lopez Martin
+// Usuario del Juez: F36
 
 
-template <class T> class BinTree {
-public:
-	// ...
-	bool empty() const;
-	const T& root() const;
-	BinTree left() const;
-	BinTree right() const;
-	pair<bool, int> num_nodos(BinTree arbol) const;
-private:
-	// ...
-};
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "bintree.h"
 
-template<typename T>
-bool num_nodos(const BinTree<T>& t) {
-	if (t.empty()) {
-		return 0;
-	}
-	else {
-		return 1 + num_nodos(t.left()) + num_nodos(t.right());
-	}
+using namespace std;
+
+
+pair<bool, int> es_zurdo(const BinTree<char>& tree) {
+    //devuelve si es zurdo, y numNodos de ese arbol
+    if (tree.empty()) {//es un arbol vacio -> es zurdo
+        return { true, 0 };
+    }
+    else if(tree.left().empty() && tree.right().empty()){//es una hoja --> es zurdo
+        return { true, 1 };
+    }
+    else {
+        auto [esz_izq, num_nodos_izq] = es_zurdo(tree.left());
+        auto [esz_der, num_nodos_der] = es_zurdo(tree.right());
+
+        bool zurdo = esz_izq && esz_der &&
+            num_nodos_izq > num_nodos_der;
+        int numNodos = 1 + num_nodos_izq + num_nodos_der;
+
+        return {zurdo, numNodos};
+    }
 }
 
 
 
+// función que resuelve el problema
+bool resolver(const BinTree<char> arbol) {
+    pair<bool, int> sol = es_zurdo(arbol);
+    return sol.first;
+}
 
-template<typename T>
-pair<bool, int> es_zurdo(const BinTree<T>& t) {
-	if (t.empty()) { //es un arbol vacio
-		return {true, 0};
-	}
-	else if(t.left().empty() && t.right().empty()){// es una hoja
-		return {true, 1};
-	}
-	auto [es_zurdo_izq, num_nodos_izq] = es_zurdo(t.left());
-	auto [es_zurdo_der, num_nodos_der] = es_zurdo(t.right());
-	bool es_zurdo = es_zurdo_izq && es_zurdo_der
-		&& num_nodos_izq > num_nodos_der;
-	int num_nodos = 1 + num_nodos_izq + num_nodos_der;
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    BinTree<char> arbol = BinTree<char>();
+    arbol = read_tree<char>(cin);
+
+
+    bool sol = resolver(arbol);
+    // escribir sol
+    if (sol) {
+        cout << "SI" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
 }
