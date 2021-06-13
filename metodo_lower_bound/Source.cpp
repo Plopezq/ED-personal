e22093b30dcce3570266a3bd21444181e3cafb7e
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2021-06-13 17:45:26 +0200

    TODO -> metodo lower bound juez

--- metodo_lower_bound/Source.cpp
+++ metodo_lower_bound/Source.cpp
@@ -1,310 +1,78 @@
-﻿/*
- * ---------------------------------------------------
- *                ESTRUCTURAS DE DATOS
- * ---------------------------------------------------
- *              Facultad de Informática
- *         Universidad Complutense de Madrid
- * ---------------------------------------------------
- */
+﻿
+// Nombre del alumno: Pablo Lopez Martin
+// Usuario del Juez: F36
 
- /*
-  * MUY IMPORTANTE: Para realizar este ejercicio solo podéis
-  * modificar el código contenido entre las etiquetas <answer>
-  * y </answer>. Toda modificación fuera de esas etiquetas no será
-  * corregida.
-  *
-  * Tampoco esta permitido modificar las líneas que contienen
-  * las etiquetas <answer> y </answer>, obviamente :-)
-  */
-
-  //@ <answer>
-  /*
-   * Introduce el nombre y apellidos de los/as componentes del grupo:
-   *
-   * Estudiante 1:
-   * Estudiante 2:
-   *
-   */
-   //@ </answer>
 
 #include <iostream>
-#include <cassert>
+#include <iomanip>
 #include <fstream>
-
-template <typename T> class SetTree {
-public:
-    SetTree() : root_node(nullptr), num_elems(0) {}
-    SetTree(const SetTree& other)
-        : num_elems(other.num_elems), root_node(copy_nodes(other.root_node)) {}
-    ~SetTree() { delete_nodes(root_node); }
-
-    void insert(const T& elem) {
-        auto [new_root, inserted] = insert(root_node, elem);
-        root_node = new_root;
-        if (inserted) {
-            num_elems++;
-        }
-    }
-
-    bool contains(const T& elem) const { return search(root_node, elem); }
-
-    void erase(const T& elem) {
-        auto [new_root, removed] = erase(root_node, elem);
-        root_node = new_root;
-        if (removed) {
-            num_elems--;
-        }
-    }
-
-    int size() const { return num_elems; }
-    bool empty() const { return num_elems == 0; }
-
-    SetTree& operator=(const SetTree& other) {
-        if (this != &other) {
-            num_elems = other.num_elems;
-            delete_nodes(root_node);
-            root_node = copy_nodes(other.root_node);
-        }
-        return *this;
-    }
-
-    void display(std::ostream& out) const {
-        out << "{";
-        display(root_node, out);
-        out << "}";
-    }
-
-    // Nuevo método a implementar. La implementación se encuentra más adelante.
-    std::pair<bool, T> lower_bound(T const& e) const;
-private:
-    struct Node {
-        T elem;
-        Node* left, * right;
-
-        Node(Node* left, const T& elem, Node* right)
-            : left(left), elem(elem), right(right) {}
-    };
-
-    Node* root_node;
-    int num_elems;
-
-    static Node* copy_nodes(const Node* node) {
-        if (node == nullptr) {
-            return nullptr;
-        }
-        else {
-            return new Node(copy_nodes(node->left), node->elem,
-                copy_nodes(node->right));
-        }
-    }
-
-    static void delete_nodes(const Node* node) {
-        if (node != nullptr) {
-            delete_nodes(node->left);
-            delete_nodes(node->right);
-            delete node;
-        }
-    }
-
-    static std::pair<Node*, bool> insert(Node* root, const T& elem) {
-        if (root == nullptr) {
-            return { new Node(nullptr, elem, nullptr), true };
-        }
-        else if (elem < root->elem) {
-            auto [new_root_left, inserted] = insert(root->left, elem);
-            root->left = new_root_left;
-            return { root, inserted };
-        }
-        else if (root->elem < elem) {
-            auto [new_root_right, inserted] = insert(root->right, elem);
-            root->right = new_root_right;
-            return { root, inserted };
-        }
-        else {
-            return { root, false };
-        }
-    }
-
-    static bool search(const Node* root, const T& elem) {
-        if (root == nullptr) {
-            return false;
-        }
-        else if (elem == root->elem) {
-            return true;
-        }
-        else if (elem < root->elem) {
-            return search(root->left, elem);
-        }
-        else {
-            return search(root->right, elem);
-        }
-    }
-
-    static std::pair<Node*, bool> erase(Node* root, const T& elem) {
-        if (root == nullptr) {
-            return { root, false };
-        }
-        else if (elem < root->elem) {
-            auto [new_root_left, erased] = erase(root->left, elem);
-            root->left = new_root_left;
-            return { root, erased };
-        }
-        else if (root->elem < elem) {
-            auto [new_root_right, erased] = erase(root->right, elem);
-            root->right = new_root_right;
-            return { root, erased };
-        }
-        else {
-            return { remove_root(root), true };
-        }
-    }
-
-    static Node* remove_root(Node* root) {
-        Node* left_child = root->left, * right_child = root->right;
-        delete root;
-        if (left_child == nullptr && right_child == nullptr) {
-            return nullptr;
-        }
-        else if (left_child == nullptr) {
-            return right_child;
-        }
-        else if (right_child == nullptr) {
-            return left_child;
-        }
-        else {
-            auto [lowest, new_right_root] = remove_lowest(right_child);
-            lowest->left = left_child;
-            lowest->right = new_right_root;
-            return lowest;
-        }
-    }
-
-    static std::pair<Node*, Node*> remove_lowest(Node* root) {
-        assert(root != nullptr);
-        if (root->left == nullptr) {
-            return { root, root->right };
-        }
-        else {
-            auto [removed_node, new_root_left] = remove_lowest(root->left);
-            root->left = new_root_left;
-            return { removed_node, root };
-        }
-    }
-
-    static void display(Node* root, std::ostream& out) {
-        if (root != nullptr) {
-            if (root->left != nullptr) {
-                display(root->left, out);
-                out << ", ";
-            }
-            out << root->elem;
-            if (root->right != nullptr) {
-                out << ", ";
-                display(root->right, out);
-            }
-        }
-    }
-};
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const SetTree<T>& set) {
-    set.display(out);
-    return out;
-}
+#include "settree.h"
 
 
 using namespace std;
 
-// --------------------------------------------------------------
-//@ <answer>
+// función que resuelve el problema
+pair<bool, int>  resolver(const SetTree<int> &conjunto,const int &aux) {
 
-//
-// Implementa a continuación el método pedido
-// ¡No te olvides de indicar el coste!
-//
-
-
-template <typename T>
-std::pair<bool, T> SetTree<T>::lower_bound(T const& e) const
-{
-
-    //que dado un elemento e devuelva si existen en el conjunto elementos mayores o iguales que e 
-    // y en caso aﬁrmativo cual es el menor de ellos.
-    //El arbol es vacio
-    if ( ){
-        return std::pair<true, >
-    }
-
-
-
-
-
-
-
-    return std::pair<bool, T>();
+    pair<bool, int> sol = conjunto.lower_bound(aux);
+    return sol;
 }
 
-// 
-// Si necesitas métodos auxiliares privados, puedes poner su cabecera en la
-// definición de la clase SetTree (aunque esté fuera de las etiquetas <answer>..</answer>)
-// pero su implementación ha de estar aquí
-//
-
-// ... Métodos auxiliares ...
-
-
-// 
-// Implementa a continuación la función que trata un caso de prueba.
-//
-
-bool tratar_caso() {
-    // Leemos los elementos del conjunto
-    int num_elems;
-    cin >> num_elems;
-    if (num_elems == 0) { //La entrada termina con un 0
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int numElem;
+    cin >> numElem;
+    if (numElem == 0)
         return false;
-    }
-    //Leo todo el conjunto y lo creo
+
     SetTree<int> conjunto;
     int aux;
-    for (int i = 0; i < num_elems; i++) {
+    for (int i = 0; i < numElem; i++) {
         cin >> aux;
         conjunto.insert(aux);
     }
-    //Leo el numero de elementos a buscar
-    int numElem;
-    cin >> numElem;
-    for (int i = 0; i < numElem; i++) { //Voy buscando los distintos valores en el conjunto
-        cin >> aux;
-        auto [encontrado, valor] = conjunto.lower_bound(aux);
-            // llamo a lower bound y le paso el numero a buscar
-        if(!encontrado){
-            cout << "NO HAY" << endl;
+    int preguntas;
+    cin >> preguntas;
+    int aux2;
+    for (int i = 0; i < preguntas; i++) {
+        cin >> aux2;
+        pair<bool, int> sol = resolver(conjunto, aux2);
+
+        if (sol.first) {
+            cout << sol.second << endl;
         }
         else {
-            cout << valor << endl;
+            cout << "NO HAY" << endl;
         }
     }
+    cout << "---" << endl;
+
+
     return true;
+
 }
 
 
-//@ </answer>
-// --------------------------------------------------------------
-
-
 int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
 #ifndef DOMJUDGE
     std::ifstream in("sample.in");
-    auto cinbuf = std::cin.rdbuf(in.rdbuf());
-#endif
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
 
-    while (tratar_caso()) {}
 
-#ifndef DOMJUDGE
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
     std::cin.rdbuf(cinbuf);
-    // Descomentar si se trabaja en Windows
-    // system("PAUSE");
+    system("PAUSE");
 #endif
+
     return 0;
 }
