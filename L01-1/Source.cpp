cba61a5b25b4a8f9ae32b6316ac25b9bf3844d82
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2021-06-14 09:40:31 +0200

    ejercicio L02 hecho

---
+++ L01-1/Source.cpp
@@ -0,0 +1,44 @@
+/*
+ * ---------------------------------------------------
+ *                ESTRUCTURAS DE DATOS
+ * ---------------------------------------------------
+ *              Manuel Montenegro Montes
+ *              Facultad de Informática
+ *         Universidad Complutense de Madrid
+ * ---------------------------------------------------
+ */
+
+ /*
+  * Introduce el nombre y apellidos de los/as componentes del grupo:
+  *
+  * Estudiante 1: Pablo Lopez
+  *
+  */
+
+#include <iostream>
+#include <cassert>
+#include <fstream>
+
+
+  /* Define la clase 'Polinomio' aquí */
+
+
+bool tratar_caso() {
+
+
+}
+
+int main() {
+#ifndef DOMJUDGE
+	std::ifstream in("sample.in");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (tratar_caso()) {}
+
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+	return 0;
+}
